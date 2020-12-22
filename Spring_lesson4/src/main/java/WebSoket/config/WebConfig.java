cf4b4c7e8540ecd9e672553f7acb02fac6617e58
AvdIlay <ilay963852@mail.ru>
2020-12-22 22:40:53 +0300

    Lesson4

---
+++ Spring_lesson4/src/main/java/WebSoket/config/WebConfig.java
@@ -0,0 +1,29 @@
+package WebSoket.config;
+
+import WebSoket.domain.BurdenDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Autowired
+    private SimpMessagingTemplate template;
+
+    @Scheduled(fixedRate = 5000L)
+    private void sendItem() {
+        template.convertAndSend("/topic", "Test message");
+        template.convertAndSend("/topic", new BurdenDto(1, "Hellow  Ilya!"));
+    }
+
+}
